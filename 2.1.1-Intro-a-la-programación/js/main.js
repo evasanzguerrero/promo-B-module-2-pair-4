1b098aebedd6cec33e7d092ca9be89d86b9f3281
evasanzgue <evasanzgue@gmail.com>
2024-06-02 10:31:31 +0200

    Ejercicio 1. Mensaje de navegador obsoleto

---
+++ 2.1.1-Intro-a-la-programación/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const titulo = document.querySelector('h1');
+titulo.innerHTML = '<p class = parrafo>Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.</p>'
